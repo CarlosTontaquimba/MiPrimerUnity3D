9b056b11f0cd2212df7473d997d64c62c80bb7eb
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-06-01 13:56:11 -0500

    First commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+   //ESTE SCRIPT ES PARA QUE LA CÁMARA SIGA EL JUGADOR
+   public GameObject player;
+   //Tecnica de offset 
+   public  Vector3 offset = new Vector3(0, 5, -6);
+   private void Update()
+   {
+      //this.transform.position = posicion actual del transform de la camara
+      this.transform.position = player.transform.position +  offset; // Copiamos el valor de la posicion del transform del player a la posicion del transform de la camara
+   }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // El Range hace que se pueda crear un rango de velocidad entre 0 y 20 y el SerializeField hace que 
+    // a pesar de que la variable sea privada, pueda aparecer en el editor y modificarla
+    //Tooltip muestra un mensaje en el editor con el mensaje escrito
+    [Range(0, 20), 
+    SerializeField,
+    Tooltip("Velocidad lineal máxima del coche")]
+    private float spped = 10f;
+    [Range(0, 90), 
+     SerializeField,
+     Tooltip("Velocidad de giro máxima del coche")]
+    private float turnSpeed = 80f; // Velocidad de giro
+    
+    private float horizontalInput, verticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Debug.Log("Movimiento horizontal : "+horizontalInput);
+        //Movimiento
+        transform.Translate(spped*Time.deltaTime*Vector3.forward * verticalInput);
+        //Giro
+        transform.Rotate(turnSpeed * Time.deltaTime * Vector3.up * horizontalInput);
+    }
+    
+}
